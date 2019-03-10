5816323518cb9c6c3e987cb37a2ec099f3d83de9
Emily Riederer <emilyriederer@gmail.com>
2019-03-10 14:16:59 -0500

    renamed text-templates -> templates

--- generate-email.R
+++ generate-email.R
@@ -1,7 +1,7 @@
 generate_email <- function(name, message = c("apply", "accept", "decline")){
   
   match.arg(message)
-  file <- paste0("text-templates/", message, ".txt")
+  file <- paste0("templates/", message, ".txt")
   header <- "<h2> <img src = {logo} height = 50, width = 50, style = 'vertical-align:text-bottom'> {event_name} </h2>"
   footer <- "<a href = '{event_link}' >{event_name}</a><p> Email sent on {current_date_time}. </p>"
   
